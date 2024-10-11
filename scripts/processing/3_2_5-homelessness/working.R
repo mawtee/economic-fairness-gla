cf686b309569bfd157762023ff58413494bdd5dc
mawtee <mawtibbles@gmail.com>
2024-10-11 16:57:28 +0100

    scraper for rough sleeping ready - file structure needs to be set up still

--- scripts/processing/3_2_5-homelessness/working.R
+++ scripts/processing/3_2_5-homelessness/working.R
@@ -10,8 +10,8 @@ scrape_chain_data <- function(url, release_year) {
   #'
   #' @noRd
   print(paste0("Scraping chain data for ", release_year))
-  url <- 'https://data.london.gov.uk/dataset/chain-reports'
-  release_year <- '2023/24'
+  #url <- 'https://data.london.gov.uk/dataset/chain-reports'
+  #release_year <- '2023_24'
   
   # Load HTML page
   page <- rvest::read_html(url)
@@ -23,13 +23,14 @@ scrape_chain_data <- function(url, release_year) {
     str_subset("https") %>%
     str_subset(paste0(str_sub(release_year, -2, -1),'.ods'))
   
-  # Download and unzip
+  # Download and write to file
   temp <- tempfile()
   download.file(url=link, temp, mode = "wb")
-  if (dir.exists(paste0('data/raw-data/2_2_6-apprenticeships/batch-', release_year))) {
+  df <- read_ods(temp, sheet='P1')
+  if (dir.exists(paste0('data/raw-data/3_2_5-homelessness/', release_year))) {
     user_confirm <- readline("Directory for data update already exists. Are you sure you want to overwrite the existing directory? (y/n)")
     if (user_confirm=='y') {
-      unzip(zipfile=temp, exdir=paste0('data/raw-data/2_2_6-apprenticeships/batch-', release_year), overwrite=TRUE)
+      write_csv(df, paste0('data/raw-data/3_2_5-homelessness/', release_year,'/CHAIN_rough_sleeping.csv'))
       unlink(temp)
     }
     else {
@@ -37,10 +38,14 @@ scrape_chain_data <- function(url, release_year) {
         'Aborting update: user does does not want to overwrite existing directory'
       )
     }
-  }
+  } 
   else {
-    unzip(zipfile=temp, exdir=paste0('data/raw-data/2_2_6-apprenticeships/', release_year), overwrite=TRUE)
+  #if (!dir.exists(paste0('data/raw-data/3_2_5-homelessness/', release_year))) {
+    dir.create(paste0('data/raw-data/3_2_5-homelessness/', release_year))
+    write_csv(df, paste0('data/raw-data/3_2_5-homelessness/', release_year,'/CHAIN_rough_sleeping.csv'))
     unlink(temp)
   }
   
 }
+
+scrape_chain_data(url, release_year)
